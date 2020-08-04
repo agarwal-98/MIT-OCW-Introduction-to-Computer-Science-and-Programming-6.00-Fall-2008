b719e904b20e9745814588254b38a3a530aae08e
agarwal-98 <iraagarwal98@gmail.com>
2020-08-04 21:20:08 +0100

    Problem and solution set

---
+++ Problem Set 0/PS0_Solution.py
@@ -0,0 +1,11 @@
+>>> x = raw_input('what is your last name?')
+what is your last name? Agarwal
+>>> y = raw_input('what is your first name?')
+what is your first name? Ira
+>>> print y
+>>> print x
+
+#  print ('Hello' + y + x)
+
+#  print ('Hello ' + y + ' ' + x)
+# Hello Ira Agarwal
